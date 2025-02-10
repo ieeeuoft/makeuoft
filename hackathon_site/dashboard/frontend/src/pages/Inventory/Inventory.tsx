c7130a6650074aa69d44e483197a76623dd21590
Mustaballer <mus2003.abdul@gmail.com>
2025-02-09 21:46:15 -0500

    add dependency to useEffect to handle React warning

--- hackathon_site/dashboard/frontend/src/pages/Inventory/Inventory.tsx
+++ hackathon_site/dashboard/frontend/src/pages/Inventory/Inventory.tsx
@@ -66,7 +66,7 @@ const Inventory = () => {
             dispatch(getCurrentTeam());
             dispatch(getTeamOrders());
         }
-    }, [dispatch]);
+    }, [dispatch, userType]);
 
     return (
         <>
