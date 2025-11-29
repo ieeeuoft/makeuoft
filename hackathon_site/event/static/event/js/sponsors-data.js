8430cd95fd4ed6228f2c7c087e79224948b01174
Aaron Gu <gu20041006@gmail.com>
2025-11-29 16:53:08 -0500

    Add new sponosor's js components

---
+++ hackathon_site/event/static/event/js/sponsors-data.js
@@ -0,0 +1,18 @@
+// Sponsors data configuration
+// Easy to extend - just add more objects to this array
+// Note: Logo paths should be relative to the static files directory
+// The static URL prefix will be set from the template
+window.SPONSORS_DATA = [
+    {
+        name: "Qualcomm",
+        logo: "event/images/sponsors/qualcomm.png",
+        website: "https://www.qualcomm.com"
+    },
+    // Add more sponsors here:
+    // {
+    //     name: "Company Name",
+    //     logo: "event/images/sponsors/company-logo.png",
+    //     website: "https://www.company.com"
+    // },
+];
+
