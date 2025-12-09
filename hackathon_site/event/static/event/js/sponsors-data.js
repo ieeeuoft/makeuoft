7fcb2200ac9c3e4c314e006df20b5e3a5506ee59
Aaron Gu <gu20041006@gmail.com>
2025-12-09 13:56:29 -0500

    Added sponsers, update dates in FAQ

--- hackathon_site/event/static/event/js/sponsors-data.js
+++ hackathon_site/event/static/event/js/sponsors-data.js
@@ -8,6 +8,46 @@ window.SPONSORS_DATA = [
         logo: "event/images/sponsors/qualcomm.png",
         website: "https://www.qualcomm.com"
     },
+    {
+        name: "Alphawave Semi",
+        logo: "event/images/sponsors/alphawave.png",
+        website: "https://awavesemi.com"
+    },
+    {
+        name: "AMD",
+        logo: "event/images/sponsors/amd.png",
+        website: "https://www.amd.com"
+    },
+    {
+        name: "Indomie",
+        logo: "event/images/sponsors/indomie.png",
+        website: "https://www.indomie.com"
+    },
+    {
+        name: "Marvell",
+        logo: "event/images/sponsors/marvell.png",
+        website: "https://www.marvell.com"
+    },
+    {
+        name: "Microchip",
+        logo: "event/images/sponsors/microchip.png",
+        website: "https://www.microchip.com"
+    },
+    {
+        name: "MLH",
+        logo: "event/images/sponsors/mlh.png",
+        website: "https://mlh.io"
+    },
+    {
+        name: "Titan Haptics",
+        logo: "event/images/sponsors/titan.png",
+        website: "https://titanhaptics.com"
+    },
+    {
+        name: "Ubisoft",
+        logo: "event/images/sponsors/ubisoft.png",
+        website: "https://www.ubisoft.com"
+    },
     // Add more sponsors here:
     // {
     //     name: "Company Name",
